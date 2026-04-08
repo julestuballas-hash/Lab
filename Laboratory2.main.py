a08197ef82796cff7e2cbdabd1032f793677e48c
julestuballas-hash <julestuballas@gmail.com>
2026-04-08 20:30:54 +0800

    Create Laboratory2.main.py
    Late

---
+++ Laboratory2.main.py
@@ -0,0 +1,33 @@
+#Problem: Create a python program
+
+name = ""
+age = 0
+address = ""
+birthday = ""
+hobby = ""
+status = ""
+food = ""
+game = ""
+sex = ""
+
+name = input("Please enter you name: ")
+age = input("Please enter your age: ")
+address = input("Please enter your address:")
+birthday = input("Please enter your birthday: ")
+color = input("Please enter your color: ")
+hobby = input("Please enter you hobby: ")
+status = input("Please enter your status: ")
+food = input("Please enter your food: ")
+game = input("Please enter your game: ")
+sex = input("Please enter your sex: ")
+
+print("name: ", name)
+print("age: ", age)
+print("address: ", address)
+print("birthday: ", birthday)
+print("color: ", color)
+print("hobby: ", hobby)
+print("status: ", status)
+print("food: ", food)
+print("game: ", game)
+print("sex: ", sex)
